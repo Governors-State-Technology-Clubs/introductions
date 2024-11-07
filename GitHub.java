2d0bd71f4cd93d433c5f44251813e1da0259d1a5
Joseph Udani <joeudani@gmail.com>
2024-11-07 17:41:51 -0600

    Let's try this again.

---
+++ GitHub.java
@@ -0,0 +1,6 @@
+public class GitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Joe.");
+        System.out.println("Nice to meet you guys!");
+    }
+}
